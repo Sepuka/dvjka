5498b4195b14a78f1a4c75ce8c1ddbc37720ce55
shlomin <shlomin@1mps.ru>
2013-06-03 16:23:54 +0400

    Поправил шаблон залогиненного клиента на главной странице

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
+
+define('HOST', 'dvjk');
+
 if (! empty($_COOKIE['phone'])) {
     $index = file_get_contents('tmpl/index.tmpl');
+    $index = str_replace(
+        array('{PHONE}'),
+        array($_COOKIE['phone']),
+        $index);
 } else {
     $index = file_get_contents('tmpl/indexnew.tmpl');
 }
+
+$index = str_replace(
+        array('{HOST}'),
+        array(HOST),
+        $index);
 
 echo $index;
