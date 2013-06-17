f065089c903a0b366d3bc01e79d89e6f46597bf2
shlomin <shlomin@1mps.ru>
2013-06-17 11:57:39 +0400

    6 часовая блокировка по крону

--- cron.php
+++ cron.php
@@ -9,12 +9,19 @@ $db = DB::getInstance();
 
 switch ($argv[1])
 {
+    // Неоплаченные заявки
     case 'noPaymentBlock':
         $query = sprintf('UPDATE `DVJK_users` LEFT JOIN `%spayments` on `%susers`.`Id`=`%spayments`.`Sender_id`
             SET `Enabled`=0
-            WHERE `%spayments`.`Id` IS NULL AND TIMESTAMPDIFF(SECOND, `%susers`.`DateTimeCreate`, NOW()) > %s;',
-            $settings['db']['PREFIX'], $settings['db']['PREFIX'],
-            $settings['db']['PREFIX'], $settings['db']['PREFIX'],
-            $settings['db']['PREFIX'], $settings['cron']['noPaymentClientTime']);
+            WHERE TIMESTAMPDIFF(SECOND, `%susers`.`DateTimeCreate`, NOW()) > %s AND
+            (`%spayments`.`Id` IS NULL OR `%spayments`.`Complete`=0);',
+            $settings['db']['PREFIX'], $settings['db']['PREFIX'], $settings['db']['PREFIX'],
+            $settings['db']['PREFIX'], $settings['cron']['noPaymentClientTime'],
+            $settings['db']['PREFIX'], $settings['db']['PREFIX']);
+        $db->getConn()->query($query);
+        $query = sprintf('DELETE FROM `%spayments` WHERE TIMESTAMPDIFF(SECOND, `%spayments`.`DateTimeCreate`, NOW()) > %s
+            AND `%spayments`.`Complete`=0;',
+            $settings['db']['PREFIX'], $settings['db']['PREFIX'], $settings['cron']['noPaymentClientTime'],
+            $settings['db']['PREFIX']);
         $db->getConn()->query($query);
 }
