319530eb95d1a11dff345ee5a98ad2f1e055eb50
shlomin <shlomin@1mps.ru>
2013-07-05 09:59:53 +0400

    bugfix изменил имя хоста из переменной апача

--- about.php
+++ about.php
@@ -8,11 +8,11 @@ if (! empty($_COOKIE['phone'])) {
     $index = file_get_contents('tmpl/about.tmpl');
     $index = str_replace(
         array('{PHONE}', '{HOST}', '{IP}', '{YOUSELF_DONATED}', '{4YOU_DONATED}', '{REF}', '{LINK}'),
-        array($_COOKIE['phone'], $settings['site']['host'], $_SERVER['REMOTE_ADDR'],
+        array($_COOKIE['phone'], $_SERVER['HTTP_HOST'], $_SERVER['REMOTE_ADDR'],
             youself_donated($sender), you_donated($sender), getRef($sender), $sender->Link),
         $index);
 } else {
-    Header('Location: http://'. $settings['site']['host'], true, 302);
+    Header('Location: http://'. $_SERVER['HTTP_HOST'], true, 302);
 }
 
 echo $index;
